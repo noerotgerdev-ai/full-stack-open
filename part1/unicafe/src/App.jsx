4940ada01541c5e0cb85e74011dacdf0558bdf91
Noelia Rotger <noeliarotgerdev@gmail.com>
2025-10-21 20:33:02 +0200

    part1 unicafe 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,6 +14,10 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
 
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 :  (good * 100) / all;
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -27,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
