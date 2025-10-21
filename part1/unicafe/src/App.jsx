c709bdb7447f4db62b1beeb65b84cc404984a763
Noelia Rotger <noeliarotgerdev@gmail.com>
2025-10-21 20:45:41 +0200

    part1 unicafe 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,25 +4,33 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = all === 0 ? 0 : (good - bad) / all;
   const positive = all === 0 ? 0 : (good * 100) / all;
 
   if (all === 0) {
-    return <p>No feedback given</p>
+    return <p>No feedback given</p>;
   }
 
   return (
     <div>
       <h2>statistics</h2>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </div>
   );
 };
@@ -39,7 +47,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Give feedback</h1>
+      <h1>give feedback</h1>
 
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
