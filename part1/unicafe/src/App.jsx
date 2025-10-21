254c269edf5aca58a56b03770a3e375fa3d5e7c2
Noelia Rotger <noeliarotgerdev@gmail.com>
2025-10-21 21:07:21 +0200

    part1 unicafe 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,16 +6,17 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
-  const average = all === 0 ? 0 : (good - bad) / all;
-  const positive = all === 0 ? 0 : (good * 100) / all;
+  const average = (all === 0 ? 0 : (good - bad) / all).toFixed(2);
+  const positive = (all === 0 ? 0 : (good * 100) / all).toFixed(2);
 
   if (all === 0) {
     return <p>No feedback given</p>;
@@ -25,12 +26,16 @@ const Statistics = ({ good, neutral, bad }) => {
     <div>
       <h2>statistics</h2>
 
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + "%"} />
+        </tbody>
+      </table>
     </div>
   );
 };
