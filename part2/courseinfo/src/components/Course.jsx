c4864dea6eee6aa6698d31794ddce425ed15c6b3
Noelia Rotger <noerotgerdev@gmail.com>
2025-10-22 16:02:55 +0200

    part2 courseinfo 2.5 (refactor)

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,20 +2,18 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map((course) => {
-        return (
-          <div key={course.id}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total total={course.parts} />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const Course = ({ courses }) => (
+  <div>
+    {courses.map((course) => {
+      return (
+        <div key={course.id}>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total total={course.parts} />
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default Course;
